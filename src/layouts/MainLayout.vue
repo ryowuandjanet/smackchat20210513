18a9061553a4213aee17565c1a7ac0034dd087ea
ryowuandjanet <ryowudata@gmail.com>
2021-05-13 17:23:32 +0800

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,48 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        
+        <q-btn 
+          v-if="$route.fullPath.includes('/chat')"
+          v-go-back.single
+          flat
+          dense 
+          icon="arrow_back" 
+          label="BACK" />
+
+        <q-toolbar-title class="absolute-center">
+          {{ title }} 
+        </q-toolbar-title>
+
+        <q-btn 
+          to="/auth"
+          class="absolute-right q-pr-sm"
+          no-caps
+          flat
+          dense 
+          icon="account_circle" 
+          label="Login" />
+
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+  export default {
+    computed: {
+      title() {
+        console.log(this.$route)
+        let currentPath = this.$route.fullPath
+        if (currentPath == '/') return 'SmackChat'
+        else if (currentPath == '/chat') return 'Chat'
+        else if (currentPath == '/auth') return 'Login'
+      }
+    }
+  }
+</script>
